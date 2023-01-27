0d0ab6efb9cb7ec9291afe0397c2e1b04bbcc42d
Elliot Thomas <elliot@eth0net.io>
2023-01-27 15:28:02 +0000

    Add animal hemogen pack and operation

---
+++ Source/AnimalHemogen/AnimalHemogen.cs
@@ -0,0 +1,10 @@
+﻿using Verse;
+
+namespace AnimalHemogen
+{
+    /// <summary>
+    /// AnimalHemogen static class to load up the mod.
+    /// </summary>
+    [StaticConstructorOnStartup]
+    public static class AnimalHemogen { }
+}
---
+++ Source/AnimalHemogen/Recipe_ExtractAnimalHemogen.cs
@@ -0,0 +1,17 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
+
+namespace AnimalHemogen
+{
+    internal class Recipe_ExtractAnimalHemogen : Recipe_ExtractHemogen
+    {
+        protected override void OnSurgerySuccess(Pawn pawn, BodyPartRecord part, Pawn billDoer, List<Thing> ingredients, Bill bill)
+        {
+            if (!GenPlace.TryPlaceThing(ThingMaker.MakeThing(ThingDef.Named("AnimalHemogenPack"), null), pawn.PositionHeld, pawn.MapHeld, ThingPlaceMode.Near, null, null, default(Rot4)))
+            {
+                Log.Error("Could not drop animal hemogen pack near " + pawn.PositionHeld);
+            }
+        }
+    }
+}
