d6e0687db50995d5d360880c4371abe5cfb81458
Elliot Thomas <elliot@eth0net.io>
2023-02-02 00:34:08 +0000

    Patch hemogen extractor to take animals

---
+++ Source/AnimalHemogen/Harmony/Harmony_HemogenExtractor.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using Verse;
+
+namespace AnimalHemogen.Harmony
+{
+    internal class Harmony_HemogenExtractor
+    {
+        [HarmonyPatch("HemogenExtractorPatches", "Prefix_AddHumanlikeOrders")]
+        internal static class Harmony_HemogenExtractorPatches_Prefix_AddHumanlikeOrders
+        {
+            internal static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions) => instructions.MethodReplacer(
+                AccessTools.PropertyGetter(typeof(RaceProperties), "Humanlike"),
+                AccessTools.Method(typeof(Harmony_HemogenExtractorPatches_Prefix_AddHumanlikeOrders), "HumanlikeOrAnimal")
+            );
+            internal static bool HumanlikeOrAnimal(RaceProperties raceProps) => raceProps.Humanlike || raceProps.Animal;
+        }
+    }
+}
